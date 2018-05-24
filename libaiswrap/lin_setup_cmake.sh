a9160069eed1d521978655dfc79a8369b9e2e86d
James Thompson <james@jamtho.com>
2018-05-24 14:39:35 +0100

    Fix CMake wrapper scripts
    These (called one-off) needed to create the target dirs

--- libaiswrap/lin_setup_cmake.sh
+++ libaiswrap/lin_setup_cmake.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+mkdir -p lin_build
 cd lin_build
 cmake ../libaiswrap -DCMAKE_BUILD_TYPE=Release
 cd ..
--- libaiswrap/win_setup_cmake.bat
+++ libaiswrap/win_setup_cmake.bat
@@ -1,3 +1,4 @@
+md win_build
 cd win_build
 cmake ../libaiswrap -G"Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release
 cd ..
